a325dc1a57cbcd07a7c378f56fc75b493611abc8
Sergey Filimonov <srfilimonoff@gmail.com>
2023-01-23 17:41:46 +0300

    page navigation

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   // предотвращение прокрутки страницы в самый верх, при нажатии на кнопку логина
@@ -37,10 +39,7 @@ document.addEventListener('keydown', function (e) {
 
 /////////////////////////////////////////////////////////////////
 // 188 Implementing Smooth Scrolling // Реализация плавной прокрутки
-
-const btnScrollTo = document.querySelector(`.btn--scroll-to`);
-const section1 = document.querySelector(`#section--1`);
-
+// Кнопка скрола
 btnScrollTo.addEventListener(`click`, function (e) {
   const s1coords = section1.getBoundingClientRect();
   // console.log(s1coords);
@@ -68,6 +67,33 @@ btnScrollTo.addEventListener(`click`, function (e) {
   // });
 
   section1.scrollIntoView({ behavior: `smooth` });
+});
+
+///////////////////////////////////////
+// Page navigation // постраничная новигация
+
+// document.querySelectorAll(`.nav__link`).forEach(function (el) {
+//   // Функция обратного высзова в функ. обрат. вызова
+//   el.addEventListener(`click`, function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute(`href`);
+//     console.log(id); // в зависимости от элемента, на который нажали // #section--3
+//     document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+//   });
+// });
+
+// Делегирования событий
+// 1. Добавить слушателя событий к общему родительскому элементу // Add event listener to common parent element
+// 2. Определить, какой элемент вызвал событие // Determine what element originated the event
+
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  e.preventDefault();
+  // Стратегия подбора
+  // проверка, содеражит ли целевой элемент класс, который указан в параметре
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+  }
 });
 
 /////////////////////////////////////////////////////////////
